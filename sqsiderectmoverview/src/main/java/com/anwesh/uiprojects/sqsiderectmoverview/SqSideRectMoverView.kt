07029497cb0d398edcbe9f4f1a31a93ee568acec
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 14:51:42 +0530

    adding view in activity

--- sqsiderectmoverview/src/main/java/com/anwesh/uiprojects/sqsiderectmoverview/SqSideRectMoverView.kt
+++ sqsiderectmoverview/src/main/java/com/anwesh/uiprojects/sqsiderectmoverview/SqSideRectMoverView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.sqsiderectmoverview
 /**
  * Created by anweshmishra on 13/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val rects : Int = 4
+val sizeFactor : Float = 2.7f
+val rSizeFactor : Float = 3.0f
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
